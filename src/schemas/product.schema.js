061a30779726717346e484570c3aa8cd399a05ae
franklin rodriguez <franklinserif@gmail.com>
2022-04-13 19:14:15 -0400

    product schema

---
+++ src/schemas/product.schema.js
@@ -0,0 +1,24 @@
+const Joi = require('joi');
+
+const id = Joi.string().uuid();
+const name = Joi.string().min(3).max(15);
+const price = Joi.number().integer().min(10);
+const image = Joi.string().uri();
+
+const createProductSchema = Joi.object({
+  name: name.required(),
+  price: price.require(),
+  image: image.require(),
+});
+
+const updateProductSchema = Joi.object({
+  name: name,
+  price: price,
+  image: image,
+});
+
+const getProductSchema = Joi.object({
+  id: id.require(),
+});
+
+module.exports = { createProductSchema, updateProductSchema, getProductSchema };
