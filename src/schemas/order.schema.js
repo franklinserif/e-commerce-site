e03cd120438c3129accfe1236c0822049f48cc10
franklin rodriguez <franklinserif@gmail.com>
2022-04-13 19:09:02 -0400

    order schema

---
+++ src/schemas/order.schema.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+
+const id = Joi.number().integer();
+const custumerId = Joi.number().integer();
+const orderId = Joi.number().integer();
+const productId = Joi.number().integer();
+const amount = Joi.number().integer();
+
+const getOderSchema = Joi.object({
+  id: id.require();
+});
+
+const createOrderSchema = Joi.object({
+  customerId: customerId.require()
+});
+
+const addItemSchema = Joi.object({
+  orderId: orderId.require(),
+  productId: productId.require(),
+  amount: amount.require(),
+});
+
+module.exports = {getOderSchema, createOrderSchema, addItemSchema}
