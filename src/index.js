552c0f02c789674bff01987fbb658e45317049c6
franklin rodriguez <franklinserif@gmail.com>
2022-04-13 18:27:19 -0400

    Basic configuration and folder structure

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hi, this is my first NodeJS Express server');
+});
+
+app.get('/products', (req, res) => {
+  res.json({ name: 'Glass', price: 32.03 });
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
