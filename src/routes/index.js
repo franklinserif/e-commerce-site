13ef7347065f6c5b08bd6e91c0691db4ec2f5dc8
franklin rodriguez <franklinserif@gmail.com>
2022-04-13 20:01:08 -0400

    add routing for every specific routes

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+const express = require('expres');
+
+const productsRouter = require('./products.router');
+const categoriesRouter = require('./cateogories.router');
+const usersRouter = require('./users.router');
+const ordersRouter = require('./orders.router');
+
+function routerApi(app) {
+  const router = express.Router();
+  app.use('/api/v1', router);
+
+  router.use('/products', productsRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/users', usersRouter);
+  router.use('/orders', ordersRouter);
+}
+
+module.exports = routerApi;
