8ab7dba7c16df2f3d85a33135227cdafa600112e
franklin rodriguez <franklinserif@gmail.com>
2022-04-13 20:10:01 -0400

    Add router for main index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const cors = require('cors');
+const routerApi = require('./routes');
+const {
+  logError,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error.handler');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+const whiteList = ['http://localhost:8080', 'https://myapp.co'];
+
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  },
+};
+
+app.use(cors(options));
 
 app.use(express.json());
 
@@ -13,6 +34,12 @@ app.get('/', (req, res) => {
 app.get('/products', (req, res) => {
   res.json({ name: 'Glass', price: 32.03 });
 });
+
+routerApi(app);
+
+app.use(logError);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
